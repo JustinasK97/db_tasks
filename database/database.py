02bcfb386bf8e793cd7caa51d2a7beacc3bff174
Moksleivis <moksleivis@KITM.local>
2020-01-07 14:58:22 +0200

    database

--- database/database.py
+++ database/database.py
@@ -50,7 +50,87 @@ def exercise5():
 
 
 def exercise6():
-    query = "SELECT first_name, last_name FROM employees WHERE first_name = 6"
+    query = "SELECT first_name, last_name FROM employees WHERE LENGTH(last_name) = 6"
     db_query(query)
 
-exercise6()
+def exercise7():
+    query = "SELECT job_id, COUNT(*) FROM employees GROUP BY job_id"
+    db_query(query)
+
+# Antra dalis
+
+def exercise8():
+    query = """SELECT DISTINCT job_id FROM employees """
+
+    db_query(query)
+
+
+def exercise9():
+    query = """SELECT SUM(salary) as total_salary FROM employees"""
+
+    db_query(query)
+
+
+
+def exercise10():
+    query = """SELECT salary FROM employees
+            ORDER by salary ASC
+            LIMIT 1"""
+
+    db_query(query)
+
+def exercise11():
+    query = """SELECT salary FROM employees
+            ORDER by salary DESC
+            LIMIT 1"""
+
+    db_query(query)
+
+
+def exercise12():
+    query = """SELECT AVG(salary) as avg_salary, COUNT(employee_id) FROM employees
+            WHERE department_id = 90"""
+
+    db_query(query)
+
+
+def exercise13():
+    query = """SELECT MIN(salary), MAX(salary), SUM(salary), AVG(salary) FROM employees"""
+
+    db_query(query)
+
+
+def exercise14():
+    query = """SELECT job_id, COUNT(*)  FROM employees
+               GROUP BY job_id"""
+
+    db_query(query)
+
+
+def exercise15():
+    query = """SELECT (MAX(salary) - MIN(salary)) as DIF_salary FROM employees"""
+
+    db_query(query)
+
+
+def exercise16():
+    query = """SELECT department_id, SUM(salary) FROM employees
+               GROUP BY department_id"""
+
+    db_query(query)
+
+
+def exercise17():
+    query = """SELECT AVG(salary), job_id FROM employees
+                WHERE NOT job_id = 'IT_PROG'
+               GROUP BY job_id"""
+
+    db_query(query)
+
+
+def exercise18():
+    query = """SELECT MIN(salary), manager_id, job_id FROM employees
+                GROUP by manager_id
+               """
+
+    db_query(query)
